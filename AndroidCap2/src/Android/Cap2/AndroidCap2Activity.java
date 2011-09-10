ab6153d07e66406ec09ac8327226cdb3d832821d
William <meykos@gmail.com>
2011-09-10 10:03:15 -0300

    Primeiro Exemplo

---
+++ AndroidCap2/src/Android/Cap2/AndroidCap2Activity.java
@@ -0,0 +1,13 @@
+package Android.Cap2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidCap2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
