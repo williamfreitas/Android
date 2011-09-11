f9336ed3dcb67a3b987e3ab0302b5d9edd586594
William <meykos@gmail.com>
2011-09-11 13:27:57 -0300

    Domingo 11 de setembro

---
+++ AndroidCap2/src/Android/Cap2/Cadastro.java
@@ -0,0 +1,16 @@
+package Android.Cap2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Cadastro extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+
+}
